5ee3c003028571ae26310adf6520ffe44d22a6a6
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-11-09 08:04:38 +0000

    green test test move_backward_when_direction_is_South

--- MarsRoverKata/MarsRover.cs
+++ MarsRoverKata/MarsRover.cs
@@ -26,6 +26,7 @@ public class MarsRover
                 'E' => new Position(-1, 0),
                 'N' => new Position(0, -1),
                 'W' => new Position(1, 0),
+                'S' => new Position(0, 1),
                 _ => position
             };
             return;
