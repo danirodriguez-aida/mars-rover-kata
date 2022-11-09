0a5de5a62ed6b51ba795349288a124b296eb1f94
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-11-09 07:49:15 +0000

    red test move_forward_when_direction_is_South

--- MarsRoverTests/MarsRoverShould.cs
+++ MarsRoverTests/MarsRoverShould.cs
@@ -48,5 +48,20 @@ namespace MarsRoverTests {
             var expectedMarsRoverPosition = new Position(-1,0);
             marsRoverPosition.Should().BeEquivalentTo(expectedMarsRoverPosition);
         }
+
+        [Test]
+        public void move_forward_when_direction_is_South()
+        {
+            const char initialDirection = 'S';
+            var initialPosition = new Position(0,0);
+            var marsRover = new MarsRover(initialPosition, initialDirection);
+            var commands = new[] {'f'};
+
+            marsRover.Execute(commands);
+
+            var marsRoverPosition = marsRover.GetPosition();
+            var expectedMarsRoverPosition = new Position(0,-1);
+            marsRoverPosition.Should().BeEquivalentTo(expectedMarsRoverPosition);
+        }
     }
 }
