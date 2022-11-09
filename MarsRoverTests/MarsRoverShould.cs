59d7db3d8f246ac2c536cebebc73fe0177026ad8
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-11-09 08:06:59 +0000

    red test move_forward_when_direction_is_East_and_position_is 1,0

--- MarsRoverTests/MarsRoverShould.cs
+++ MarsRoverTests/MarsRoverShould.cs
@@ -4,18 +4,19 @@ using MarsRoverKata;
 namespace MarsRoverTests {
     public class MarsRoverShould {
 
-        [Test]
-        public void move_forward_when_direction_is_East()
+        [TestCase(0,0, 1, 0, TestName = "(0,0)")]
+        [TestCase(1,0, 2, 0, TestName = "(1,0)")]
+        public void move_forward_when_direction_is_East_and_position_is(int xInitial, int yInitial, int xFinal, int yFinal)
         {
             const char initialDirection = 'E';
-            var initialPosition = new Position(0,0);
+            var initialPosition = new Position(xInitial,yInitial);
             var marsRover = new MarsRover(initialPosition, initialDirection);
             var commands = new[] {'f'};
 
             marsRover.Execute(commands);
           
             var marsRoverPosition = marsRover.GetPosition();
-            var expectedMarsRoverPosition = new Position(1,0);
+            var expectedMarsRoverPosition = new Position(xFinal,yFinal);
             marsRoverPosition.Should().BeEquivalentTo(expectedMarsRoverPosition);
         }
 
